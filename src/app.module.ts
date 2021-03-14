90ab3e8789fcdf8a806887b6c31150aa3e35a621
hardik kheni <khenihardik1997@gmail.com>
2021-03-14 19:39:00 +0530

    env and mysql database connection added.- (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { DatabaseModule } from './database/database.module';
+
+const NODE_ENV: string = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        join(__dirname, '..', 'env', `.${NODE_ENV || 'dev'}.env`),
+        join(__dirname, '..', 'env', '.env'),
+      ],
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get<string>('DB_TYPE'),
+          host: configService.get<string>('DB_HOST'),
+          port: +configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASS'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [],
+          logging: true,
+          subscribers: [],
+          synchronize: false,
+        } as MysqlConnectionOptions),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
